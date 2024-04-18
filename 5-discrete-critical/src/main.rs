28d3289466d21e9d04a43262c8e92ba131ee9eae
briangormanly <bgormanly@gmail.com>
2024-04-18 00:08:10 -0400

    pile built powerlaw functions added

--- 5-discrete-critical/src/main.rs
+++ 5-discrete-critical/src/main.rs
@@ -9,8 +9,9 @@
   Starts with everything from 2-sandpile-basic-random
   Additions / Changes:
    * Density for each pile location
-     * Sandpile locations can have between 1 and 6 grains in the location
-     *  
+     * Sandpile locations have a capacity of 4 grains plus the output of the order of 
+     *  magnitude power-law distribution
+     * 
    * Moment
      * Gains move with a magnatude (speed) and direction 
        * Initial speed starts at 1 but kinetic energy can be transferred in collisions
@@ -24,21 +25,30 @@ extern crate rand;
 use rand::Rng;
 use std::vec::Vec;
 
-// turn on and off debug output
+/**
+ * Debug flag
+ */
 const DEBUG: bool = false;
 
 // total grains to drop
-const TOTAL_GRAINS: usize = 20;
+const TOTAL_GRAINS: usize = 100;
+
+// Power-law distribution parameters
+const ALPHA: f64 = 2.0;
+const X_MIN: f64 = 1.0;
+
 
 // total allowed demensions of the pile
-const X_SIZE: usize = 5;
-const Y_SIZE: usize = 5;
-const Z_SIZE: usize = 4;
+const X_SIZE: usize = 10;
+const Y_SIZE: usize = 10;
+const Z_SIZE: usize = 3;
 
 struct Grain {
     id: u32,
+    // current energy of the grain, 
+    // 0 if stationary, > 0 if in motion
+    // energy is transferred to other grains on impact
     energy: usize,
-    direction: usize,
 }
 
 /**
@@ -51,9 +61,17 @@ impl Grain {
     pub fn new(id: u32) -> Grain {
         Grain { 
             id, 
+            // current energy of the grain, 
+            // 0 if stationary, > 0 if in motion
+            // energy is transferred to other grains on impact
             energy: 0,
-            direction: 0, 
         }
+    }
+    pub fn impact(&mut self, energy: usize) {
+        // TODO determine the probability of energy transfer and the magnitude
+        //self.energy += energy;
+        // TODO detrmine which of the surrounding locations will be impacted 
+        // by this grains motion
     }
 }
 
@@ -73,40 +91,54 @@ struct Location {
 }
 
 impl Location {
-    pub fn new(x: i32, y: i32, z: i32) -> Self {
-        let mut rng = rand::thread_rng();
+    pub fn new(x: i32, y: i32, z: i32, rnd: &mut impl Rng ) -> Self {
+        // get the order of magnitude of a random power-law distribution
+        let additionalCap = normalizedPowerLawByOrdersOfMagnitude(rnd) as usize;
+        let additionalRes = normalizedPowerLawByOrdersOfMagnitude(rnd) as usize;
         Location {
             x,
             y,
             z,
-            capacity: rng.gen_range(1..=6),  // Corrected range to include 6
+            capacity: 4 + additionalCap,  
             grains: Vec::new(),  // Initialize as empty vector
-            resilience: rng.gen_range(1..=6),
+            resilience: 4 + additionalRes,  
         }
     }
 
     fn add_grain(&mut self, grain: Grain) {
+        // Check if the location has capacity to add a grain
         if self.grains.len() < self.capacity {
+            // the location is not full, add the grain
             self.grains.push(grain);
         } else {
+            // if full, start an avalanche
             println!("Capacity reached, cannot add more grains");
         }
     }
 }
 
 
-
+/**
+ * Model for an avalanche in the sandpile
+ * An avalanche is a collection of grains that have been preturbed and are moving
+ */
 struct Avalanche {
+    id: u32,
+    // Grains that are currently part of the avalanche
     grains: Vec<Grain>,
-    energy: usize,
+    // Current locations being impacted by the avalanche
+    locations: Vec<Location>,
+    
+    // direction of the avalanche, determines which
     direction: usize,
 }
 
 impl Avalanche {
-    pub fn new() -> Self {
+    pub fn new(id: u32) -> Self {
         Avalanche {
+            id,
             grains: Vec::new(),
-            energy: 0,
+            locations: Vec::new(),
             direction: 0,
         }
     }
@@ -114,7 +146,6 @@ impl Avalanche {
     // Changed from &self to &mut self to allow modification
     fn add_grain(&mut self, grain: Grain) {
         self.grains.push(grain);
-        self.energy += 1;  // Correctly increment the energy
     }
 }
 
@@ -124,11 +155,118 @@ impl Avalanche {
 // }
 
 fn main() {
+    // create a random number generator
+    let mut rnd = rand::thread_rng();
 
     // create a 3D array to hold the sandpile
     let mut array = vec![vec![vec![0; Z_SIZE]; Y_SIZE]; X_SIZE];
 
-    // createa Location
-    let mut location = Location::new(0, 0, 0);
+    // cerate locations for all the points in the array within the slope of criticality
+    for x in 0..X_SIZE {
+        for y in 0..Y_SIZE {
+            for z in 0..Z_SIZE {
+                // create a location
+                if x>=z && x<=X_SIZE-z-1 && y>=z && y<=Y_SIZE-z-1 {
+                    // create a location
+                    let location = Location::new(x as i32, y as i32, z as i32, &mut rnd);
+                    println!("Creating location x: {}, y: {}, z: {} has capacity {} and resilience {}", x, y, z, location.capacity, location.resilience);
 
+                }
+                else {
+                    // empty location
+                    //println!("!!!Location outside of critical slope x: {}, y: {}, z: {}", x, y, z);
+                }
+            }
+        }
+    }
+
+    
+
+    for i in 0..TOTAL_GRAINS {
+
+        // create a grain 
+        let grain = Grain::new(i as u32);
+
+        // start with center of the array
+        let mut x = X_SIZE / 2;
+        let mut y = Y_SIZE / 2;
+
+        // find the gains landing variance from center with more variance in the center
+        // using an alpha of 1.5
+        let mut xVariance = normalizedPowerLawByOrdersOfMagnitudeWithAlpha(1.4, &mut rnd);
+        let mut yVariance = normalizedPowerLawByOrdersOfMagnitudeWithAlpha(1.4, &mut rnd);
+
+        // check that the variance is within the bounds of the array
+        if xVariance > x as f64 {
+            xVariance = x as f64;
+        }
+        if yVariance > y as f64 {
+            yVariance = y as f64;
+        }
+
+        // find the gains landing direction
+        let xDirection = rnd.gen_range(0..2);
+        let yDirection = rnd.gen_range(0..2);
+   
+        // compute the new location of the grain given the variance and direction
+        if xDirection == 0 {
+            x -= xVariance as usize;
+        } else {
+            x += xVariance as usize;
+        }
+
+        if yDirection == 0 {
+            y -= yVariance as usize;
+        } else {
+            y += yVariance as usize;
+        }
+
+        if DEBUG { println!("Grain {} landed at x: {}, y: {}", i, x, y); }
+
+
+        
+        
+        
+
+    }
+
+
+
+    // createa Location
+    //let mut location = Location::new(0, 0, 0);
+
+}
+
+
+/**
+ * Mathematics and probability functions
+ */
+
+/**
+ * Arguments
+ * 'alpha' - The exponent of the distribution. 
+ * 'x_min' - The minimum value for the power-law distribution.
+ * 'rng' - A random number generator.
+ * 
+ */
+fn power_law(alpha: f64, rnd: &mut impl Rng) -> f64 {
+    let uniform_rand = rnd.gen::<f64>();  // Generates a random number between 0 and 1
+    X_MIN * (1.0 - uniform_rand).powf(-1.0 / (alpha - 1.0))
+}
+
+/**
+ * 
+ */
+pub fn normalizedPowerLawByOrdersOfMagnitude(rnd: &mut impl Rng) -> f64{
+    return normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA, rnd);
+
+}
+
+pub fn normalizedPowerLawByOrdersOfMagnitudeWithAlpha(alphaOverride: f64, rnd: &mut impl Rng) -> f64{
+    // call the power_law function
+    let value = power_law(alphaOverride, rnd);
+    // return the order of magnitude of the value
+    let orderOfMagnitude = value.log10().floor();
+    return orderOfMagnitude;
+    
 }
