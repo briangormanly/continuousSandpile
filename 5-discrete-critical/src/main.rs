1699e2d9cdda45e7214564a5c3dc56a9b5640fe3
briangormanly <bgormanly@gmail.com>
2024-04-22 10:33:07 -0400

    split into modules

--- 5-discrete-critical/src/main.rs
+++ 5-discrete-critical/src/main.rs
@@ -21,300 +21,40 @@
 
 */
 
+// external structs and functions
+use models::avalanche::Avalanche;
+use models::grain::Grain;
+use models::location::Location;
+use util::sandpileUtil::drawPile;
+use util::sandpileUtil::normalizedPowerLawByOrdersOfMagnitudeWithAlpha;
+
+// external modules
 extern crate rand;
 use rand::Rng;
 use std::vec::Vec;
 
-/**
- * Master Debug flag
- */
-const DEBUG: bool = true;
-const DEBUG_INIT: bool = true;
-const DEBUG_AVALANCHE: bool = true;
+// internal modules
+pub mod models;
+pub mod util;
 
-// total grains to drop
-const TOTAL_GRAINS: usize = 100;
-
-// minimum value (multiplier) for the power-law distribution
-// can be used to set a lower bound.
-const X_MIN: f64 = 1.0;
-// Power-law distribution parameters
-const ALPHA_MAIN: f64 = 2.0;
-const ALPHA_LANDING: f64 = 1.4;
-const ALPHA_EXTRA_ENERGY: f64 = 2.0;
-const ALPHA_AVALANCHE_SIZE: f64 = 1.2;
-
-// total allowed demensions of the pile
-const X_SIZE: usize = 10;
-const Y_SIZE: usize = 10;
-const Z_SIZE: usize = 3;
-
-// Physics constants
-const TERMINAL_FREE_FALL_SPEED: usize = 3;
-const BASE_RESILIENCE: usize = 3;
-const BASE_CAPACITY: usize = 4;
-
-#[derive(Clone)]
-struct Grain {
-    id: u32,
-    // current energy of the grain, 
-    // 0 if stationary, > 0 if in motion
-    // energy is transferred to other grains on impact
-    energy: usize,
-}
-
-/**
- * Model for a grain of sand in the system
- * Will be initialized with initial energy and direction of 0
- * which should be set for grains in motion
- */
-impl Grain {
-    // Constructor to create a new Grain with a specific id
-    pub fn new(id: u32) -> Grain {
-        Grain { 
-            id, 
-            // current energy of the grain, 
-            // 0 if stationary, > 0 if in motion
-            // energy is transferred to other grains on impact
-            energy: 0,
-        }
-    }
-    pub fn increaseEnergy(&mut self, energy: usize) {
-        self.energy += energy;
-    }
-    pub fn incrementEnergy(&mut self) {
-        self.energy += 1;
-    }
-    pub fn impact(&mut self, energy: usize) {
-        // TODO determine the probability of energy transfer and the magnitude
-        //self.energy += energy;
-        // TODO detrmine which of the surrounding locations will be impacted 
-        // by this grains motion
-    }
-}
-
-/**
- * Model for a location in the sandpile
- * Locations are static and do not move, they represent a point in the 3D space
- * They have a capacity for grains and a resilience to purturbations which is 
- * determined as a random value between 1 and 6
- */
-struct Location {
-    id: usize,
-    x: i32,
-    y: i32,
-    z: i32,
-    capacity: usize,
-    grainIds: Vec::<u32>,
-    resilience: usize,
-}
-
-impl Location {
-    pub fn new(x: i32, y: i32, z: i32, rnd: &mut impl Rng ) -> Self {
-        let id = generateXyzId(x as usize, y as usize, z as usize);
-
-        // get the order of magnitude of a random power-law distribution
-        let additionalCap = normalizedPowerLawByOrdersOfMagnitude(rnd) as usize;
-        let additionalRes = normalizedPowerLawByOrdersOfMagnitude(rnd) as usize;
-        Location {
-            id,
-            x,
-            y,
-            z,
-            capacity: BASE_CAPACITY + additionalCap,  
-            grainIds: Vec::<u32>::new(),    // Initialize as empty vector
-            resilience: BASE_RESILIENCE + additionalRes,  
-        }
-    }
-    pub fn emptySpace(x: i32, y: i32, z: i32) -> Self {
-        let id = generateXyzId(x as usize, y as usize, z as usize);
-
-        Location {
-            id,
-            x,
-            y,
-            z,
-            capacity: 0,  
-            grainIds: Vec::<u32>::new(),    // Initialize as empty vector
-            resilience: 0,  
-        }
-    }
-
-
-    // call purtubation
-    // both require:
-    //  - the impacting grains energy at impact (parameter)
-    //  - location resilience (self.resilience)
-    //  - the number of grains in the location (self.gains.len())
-    //  - the ability to add a grain to the avalanche [DO NOT HAVE]
-    // both produce:
-    //  - determining the total impact force
-    //  - determines if resilience is broken and grains avalanche
-    //  - determines the size of the avalanche if one occurs
-    //  - ensures an avalanche is not leger the total number of grains at the location
+// Constants
+use util::constants::ALPHA_LANDING;
+use util::constants::DEBUG;
+use util::constants::DEBUG_AVALANCHE;
+use util::constants::DEBUG_INIT;
+use util::constants::TOTAL_GRAINS;
+use util::constants::X_SIZE;
+use util::constants::Y_SIZE;
+use util::constants::Z_SIZE;
+use util::constants::TERMINAL_FREE_FALL_SPEED;
 
 
 
 
-    pub fn grainImpact(&mut self, grainId: u32, grainEnergy: usize, rnd: &mut impl Rng) -> Vec<u32> {
-
-        // first check the impact of the incoming grain on the location
-        let looseGrainIds = self.purtubation(grainId, grainEnergy, rnd);
-
-        // check to see if the location was perturbed
-        if looseGrainIds.len() == 0 {
-            // the location was not perturbed, add the grain
-            // Check if the location has capacity to add a grain
-            if self.grainIds.len() < self.capacity {
-                // the location is not full, add the grain
-                self.grainIds.push(grainId);
-            } else {
-                // pile is full, but was not perturbed, let the grain fall.
-                println!("Capacity reached, cannot add more grains");
-            }
-            return looseGrainIds;
-        } else {
-            // the location was perturbed, add the loose grains to the avalanche
-            return looseGrainIds;
-        }
-    }
-
-    fn purtubation(&mut self, incomingGrain: u32, incomingGrainEnergy: usize, rnd: &mut impl Rng) -> Vec<u32> {
-        // get the order of magnitude of a random power-law distribution
-        // as random additional energy representing a purtubation of the location
-        // add this value to the grains current energy
-        let additionalEnergy = normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA_EXTRA_ENERGY, rnd);
-        let tempSpeed = incomingGrainEnergy + additionalEnergy as usize;
-
-        // determine if this purturbation will cause an avalanche
-        if DEBUG && DEBUG_AVALANCHE { 
-            println!("resilience {} < total energy: {} for location {}, {}, {}", self.resilience, tempSpeed, self.x, self.y, self.z); 
-        }
-
-        if self.resilience < tempSpeed {
-            // start an avalanche
-            println!("Avalanche started at location x: {}, y: {}, z: {} which contains {} grains", self.x, self.y, self.z, self.grainIds.len());
-            // set the size of the avalanche
-            let mut avalancheSize = 2 + normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA_AVALANCHE_SIZE, rnd) as usize;
-            
-            // ensure that the base avalanche size is not larger than the number of grains
-            if self.grainIds.len() < avalancheSize {
-                avalancheSize = self.grainIds.len();
-            }
-
-            // add the perturbed grain to the avalanche
-            //avalanche.addGrain();
-
-            println!("Avalanche size: {}", avalancheSize);
-            let mut looseGrainIds: Vec<u32> = Vec::new();
-
-            // return the grains that are part of the avalanche
-            for i in 0..avalancheSize {
-                looseGrainIds.push(self.grainIds.pop().unwrap());
-            }
-            return looseGrainIds;
-
-        } else {
-            Vec::new() // Return an empty vector
-        }
-    }
-
-    //fn grainImpact(&mut self, grain: &'a Grain, avalanche: &'a mut Avalanche<'a>, rnd: &mut impl Rng) {
 
 
-    //fn grainImpact(&mut self) {
-        // // first check the impact of the incoming grain on the location
-        // self.purtubation(grain, avalanche, rnd);
-
-        // // Check if the location has capacity to add a grain
-        // if self.grains.len() < self.capacity {
-        //     // the location is not full, add the grain
-        //     self.grains.push(grain);
-        // } else {
-        //     // if full, start an avalanche
-        //     println!("Capacity reached, cannot add more grains");
-        // }
-        
-    //}
-
-    // fn purtubation(&mut self, grain: &'a Grain, avalanche: &'a mut Avalanche<'a>, rnd: &mut impl Rng) {
-    //     // get the order of magnitude of a random power-law distribution
-    //     // as random additional energy representing a purtubation of the location
-    //     // add this value to the grains current energy
-    //     let additionalEnergy = normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA_EXTRA_ENERGY, rnd);
-    //     let tempSpeed = grain.energy + additionalEnergy as usize;
-
-    //     // determine if this purturbation will cause an avalanche
-    //     if self.resilience < tempSpeed {
-    //         // start an avalanche
-    //         println!("Avalanche started at location x: {}, y: {}, z: {}", self.x, self.y, self.z);
-    //         // set the size of the avalanche
-    //         let mut baseAvalancheSize = 2 + normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA_AVALANCHE_SIZE, rnd) as usize;
-            
-    //         // ensure that the base avalanche size is not larger than the number of grains
-    //         if self.grains.len() < baseAvalancheSize {
-    //             baseAvalancheSize = self.grains.len();
-    //         }
-
-    //         // add the perturbed grain to the avalanche
-    //         avalanche.addGrain(&self.grains.pop().unwrap());
-
-    //         println!("Avalanche size: {}", avalanche.grains.len());
-
-    //     }
-    // }
-    pub fn getNumberOfGrains(&self) -> usize {
-        return self.grainIds.len();
-    }
-}
 
 
-/**
- * Model for an avalanche in the sandpile
- * An avalanche is a collection of grains that have been preturbed and are moving
- */
-struct Avalanche {
-    id: u32,
-    // Grains that are currently part of the avalanche
-    grainIds: Vec::<u32>,
-    // Current locations being impacted by the avalanche
-    locationIds: Vec::<u32>,
-    
-    // direction of the avalanche, determines which
-    direction: usize,
-}
-
-impl Avalanche {
-    pub fn new(id: u32) -> Self {
-        Avalanche {
-            id,
-            grainIds: Vec::<u32>::new(),
-            locationIds: Vec::<u32>::new(),
-            direction: 0,
-        }
-    }
-
-    pub fn addGrain(&mut self, grainId: u32) {
-        self.grainIds.push(grainId);
-    }
-
-    // Changed from &self to &mut self to allow modification
-    // pub fn addGrain(&mut self, grain: &'a Grain) {
-    //     self.grains.push(grain);
-    // }
-    // pub fn getFullAvalancheEnergy(&self) -> usize {
-    //     let mut totalEnergy = 0;
-    //     for grain in &self.grains {
-    //         totalEnergy += grain.energy;
-    //     }
-    //     return totalEnergy;
-    // }
-}
-
-// struct Location {
-//     id: usize,
-//     mass: rand::thread_rng().gen_range(1..5),
-// }
 
 fn main() {
     // create a random number generator
@@ -333,9 +73,10 @@ fn main() {
                 if x>=z && x<=X_SIZE-z-1 && y>=z && y<=Y_SIZE-z-1 {
                     // create a location
                     let location = Location::new(x as i32, y as i32, z as i32, &mut rnd);
-                    column_z.push(location);
+
+                    if DEBUG && DEBUG_INIT { println!("Creating location x: {}, y: {}, z: {} has Id {}, capacity {} and resilience {}", x, y, z, location.id, location.capacity, location.resilience); }
                     // add the location to the array
-                    //if DEBUG && DEBUG_INIT { println!("Creating location x: {}, y: {}, z: {} has capacity {} and resilience {}", x, y, z, array[x][y][z].capacity, array[x][y][z].resilience); }
+                    column_z.push(location);
                 }
                 else {
                     // empty 
@@ -432,13 +173,19 @@ fn main() {
         // see if the array location is not at capacity and fall until it is not
         if DEBUG && DEBUG_AVALANCHE { println!("Grain {} started at x: {}, y: {}, z: {}", i, x, y, z) };
 
-        while array[x][y][z].resilience == 0 || ( array[x][y][z].grainIds.len() < array[x][y][z].capacity && z > 0 ) {
+        // fall until the grain lands on a location that is not at capacity
+        // fall through any locations that are empty (resilience == 0)
+        // if not at z=0, check the location below to see if it has capacity
+        while array[x][y][z].resilience == 0 || ( z > 0 && array[x][y][z-1].grainIds.len() < array[x][y][z-1].capacity ) {
             z -= 1;
             // increase the energy of the grain up to terminal velocity
             if grains[i].energy < TERMINAL_FREE_FALL_SPEED {
                 grains[i].incrementEnergy();
             }
         }
+
+        // if z > 0, check that the locations in the neighborhood at z-1 are not at capacity
+
 
         if DEBUG && DEBUG_AVALANCHE { println!("Grain {} landed at x: {}, y: {}, z: {}", i, x, y, z); }
 
@@ -458,83 +205,3 @@ fn main() {
 
 }
 
-fn drawPile(array: &Vec<Vec<Vec<Location>>>) {
-    println!("Drawing the pile");
-    let mut grandTotal = 0;
-    for z in (0..Z_SIZE -1).rev() {
-
-        for y in 0..Y_SIZE {
-
-            print!("\n");
-
-            for x in 0..X_SIZE {
-                //print!("x:{}, y:{}, z:{} value:{}", x, y, z, array[x][y][z]);
-                print!("{}", array[x][y][z].getNumberOfGrains());
-                grandTotal += array[x][y][z].getNumberOfGrains();
-            }
-            
-        }
-        println!("\n");
-    }
-    println!(" ");
-    println!("Total grains in the pile: {}", grandTotal);
-}
-
-
-/**
- * Mathematics and probability functions
- */
-
-/**
- * Arguments
- * 'alpha' - The exponent of the distribution. 
- * 'x_min' - The minimum value for the power-law distribution.
- * 'rng' - A random number generator.
- * 
- */
-fn power_law(alpha: f64, rnd: &mut impl Rng) -> f64 {
-    let uniform_rand = rnd.gen::<f64>();  // Generates a random number between 0 and 1
-    X_MIN * (1.0 - uniform_rand).powf(-1.0 / (alpha - 1.0))
-}
-
-/**
- * 
- */
-pub fn normalizedPowerLawByOrdersOfMagnitude(rnd: &mut impl Rng) -> f64{
-    return normalizedPowerLawByOrdersOfMagnitudeWithAlpha(ALPHA_MAIN, rnd);
-
-}
-
-pub fn normalizedPowerLawByOrdersOfMagnitudeWithAlpha(alphaOverride: f64, rnd: &mut impl Rng) -> f64{
-    // call the power_law function
-    let value = power_law(alphaOverride, rnd);
-    // return the order of magnitude of the value
-    let orderOfMagnitude = value.log10().floor();
-    return orderOfMagnitude;
-    
-}
-
-fn num_bits_needed(max_value: usize) -> usize {
-    // Compute the number of bits required to store max_value
-    // This calculates the floor of the logarithm base 2 of max_value and adds 1
-    (max_value as f64).log2().ceil() as usize
-}
-
-/**
- * Generate a unique id based on the x, y, z coordinates
- */
-fn generateXyzId(x: usize, y: usize, z: usize) -> usize {
-    let max_x_index = X_SIZE - 1;
-    let max_y_index = Y_SIZE - 1;
-    let max_z_index = Z_SIZE - 1;
-
-    // Determine the number of bits needed for each dimension
-    let x_bits = num_bits_needed(max_x_index);
-    let y_bits = num_bits_needed(max_y_index);
-    let z_bits = num_bits_needed(max_z_index);
-
-    // Encode x, y, z into a single usize using bit shifts
-    // We shift x by the sum of the bits required for y and z
-    // and shift y by the bits required for z
-    (x << (y_bits + z_bits)) | (y << z_bits) | z
-}
---
+++ 5-discrete-critical/src/util/constants.rs
@@ -0,0 +1,29 @@
+/**
+ * Master Debug flag
+ */
+pub const DEBUG: bool = true;
+pub const DEBUG_INIT: bool = true;
+pub const DEBUG_AVALANCHE: bool = false;
+pub const DEBUG_LOCATION: bool = true;
+
+// total grains to drop
+pub const TOTAL_GRAINS: usize = 10;
+
+// minimum value (multiplier) for the power-law distribution
+// can be used to set a lower bound.
+pub const X_MIN: f64 = 1.0;
+// Power-law distribution parameters
+pub const ALPHA_MAIN: f64 = 2.0;
+pub const ALPHA_LANDING: f64 = 1.4;
+pub const ALPHA_EXTRA_ENERGY: f64 = 2.0;
+pub const ALPHA_AVALANCHE_SIZE: f64 = 1.2;
+
+// total allowed demensions of the pile
+pub const X_SIZE: usize = 5;
+pub const Y_SIZE: usize = 5;
+pub const Z_SIZE: usize = 2;
+
+// Physics constants
+pub const TERMINAL_FREE_FALL_SPEED: usize = 3;
+pub const BASE_RESILIENCE: usize = 3;
+pub const BASE_CAPACITY: usize = 4;
