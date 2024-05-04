221563b0ee942350ab9d5e7eafc0d9a531158741
Brian Gormanly <brian.gormanly@marist.edu>
2024-05-04 10:02:58 -0400

    data for lightning talk

--- 5-discrete-critical/src/util/constants.rs
+++ 5-discrete-critical/src/util/constants.rs
@@ -23,18 +23,18 @@ pub const ALPHA_MAIN: f64 = 1.6;
 // Amount of variation in the initial x,y coordinates of the grain as it enters the system (off center)
 pub const ALPHA_LANDING: f64 = 1.4;
 // Amount of additional energy added to the grains current energy in impact
-pub const ALPHA_EXTRA_ENERGY: f64 = 1.2;
+pub const ALPHA_EXTRA_ENERGY: f64 = 0.8;
 // Amount of additional grains to add to an avalanche in addition to the base size as determined by the avalanche method (see BASE_AVALANCHE_METHOD)
 pub const ALPHA_AVALANCHE_SIZE: f64 = 1.2;
 // Additional possible capacity of location
 pub const ALPHA_LOCATION_EXTRA_CAPACITY: f64 = 2.2;
 // Additional possible resilience of location
-pub const ALPHA_LOCATION_EXTRA_RESILIENCE: f64 = 1.15;
+pub const ALPHA_LOCATION_EXTRA_RESILIENCE: f64 = 0.8;
 
 // total allowed dimensions of the pile
-pub const X_SIZE: i32 = 61;
-pub const Y_SIZE: i32 = 61;
-pub const Z_SIZE: i32 = 37;
+pub const X_SIZE: i32 = 21;
+pub const Y_SIZE: i32 = 21;
+pub const Z_SIZE: i32 = 16;
 
 // Physics constants
 pub const TERMINAL_FREE_FALL_SPEED: usize = 3;
@@ -48,4 +48,4 @@ pub const BASE_AVALANCHE_SIZE: usize = 2;
 pub const BASE_AVALANCHE_SIZE_PERCENT: f64 = 0.75;
 
 // Total gains to be introduced into the system
-pub const TOTAL_GRAINS: usize = 10000;
+pub const TOTAL_GRAINS: usize = 100000;
